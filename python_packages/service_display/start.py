85ddb4f4b02a5574ae75f505a1ca719996d1ef38
Andrei Gramakov <mail@agramakov.me>
2021-01-02 19:47:55 +0000

    Improve device info

--- python_packages/service_display/start.py
+++ python_packages/service_display/start.py
@@ -7,34 +7,34 @@ from time import sleep
 def start():
     show_l("   Hello", wait_sec=1)
     show_l("I am Zakhar", wait_sec=2)
-    check_face(show_error=True)
-    check_motors(show_error=True)
-    check_sensors(show_error=True)
 
-    show_sl("waiting for...", "network")
-    while not check_network():
-        sleep(.1)
+    if not check_network():
+        show_sl("waiting for...", "network")
+        while not check_network():
+            sleep(.1)
 
-    show_sl("waiting for...", "ssh")
-    while not check_ssh():
-        sleep(.1)
+    if not check_ssh():
+        show_sl("waiting for...", "ssh")
+        while not check_ssh():
+            sleep(.1)
 
     while True:
+        if (check_ros()):
+            show_mm("roscore is active", "Skip device check", wait_sec=5)
+        else:
+            f = " "
+            m = " "
+            s = " "
+            if check_face():
+                f = "+"
+            if check_motors():
+                m = "+"
+            if check_sensors():
+                s = "+"
+            show_mm("Devices:", "[%s] f  [%s] m  [%s] s" % (f, m, s), wait_sec=3)
+
         net_info = get_net()
         show_mm("Host: %s" % net_info[0], "IP: %s" % net_info[1], wait_sec=1)
-        if (check_ros()):
-            show_mm("roscore is active", "skip device check", wait_sec=5)
-        else:
-            f = "x"
-            m = "x"
-            s = "x"
-            if check_face():
-                f = "v"
-            if check_motors():
-                m = "v"
-            if check_sensors():
-                s = "v"
-            show_mm("Devices:", " [%s]f   [%s]m   [%s]s" % (f, m, s), wait_sec=3)
 
         show_mm("zakhar.agramakov.me", "    GPLv3 (c) 2020", wait_sec=1)
 
